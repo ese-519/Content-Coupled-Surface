d410a796b72b94be5c393cfe5294549cc0189ad0
Kamenee <kamenee1984@gmail.com>
2016-11-08 15:31:49 -0500

    Update main.cpp
    added functionality to determine inflate point based on gathered pressure data

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 
 Serial pc(USBTX, USBRX);
 
+#define MAXCOUNT 5
+#define LEN 16
+
+float prevPressureVal[LEN][LEN]={0};
+float pressureMat[LEN][LEN] = {0};
+int counterTable[LEN][LEN] = {0};
+
 // mux
 DigitalOut select0_mux1(p24);
 DigitalOut select1_mux1(p23);
@@ -22,10 +29,10 @@ int getBit(int num, int bitNo) {
 }
 
 
-void getPressureMatrix(float matrix[16][16]){
+void getPressureMatrix(){
 
     printf("******************************\n");
-    for (int rows = 0; rows <= 15; rows++) {
+    for (int rows = 0; rows < LEN; rows++) {
     
         select0_demux1 = getBit(rows,0);
         select1_demux1 = getBit(rows,1);
@@ -33,7 +40,7 @@ void getPressureMatrix(float matrix[16][16]){
         select3_demux1 = getBit(rows,3);
     
 
-     for (int col = 0; col<= 15; col++) {
+     for (int col = 0; col < LEN; col++) {
             //pc.printf("Select Line: %d ", col);
             
             // This code will select the select lines on the mbed
@@ -42,21 +49,44 @@ void getPressureMatrix(float matrix[16][16]){
             select2_mux1 = getBit(col, 2);
             select3_mux1 = getBit(col, 3);
             //wait(0.05);
-            matrix[rows][col] = analog_read_1.read();
-            printf("%f ", matrix[rows][col]);
+            pressureMat[rows][col] = analog_read_1.read();
+            printf("%f ", pressureMat[rows][col]);
         
         }  
         printf("\n");
     }
     printf("********************************\n");
 }
+
+void ComparePressureVal(){
+    
+  for(int rows = 0; rows < LEN; rows++){
+    for(int col = 0; col < LEN; col++){
+       //TODO: the pressure might differ a bit but could be in same range. We might want to consider
+       // this situation too.
+       if(pressureMat[rows][col] == prevPressureVal[rows][col]){
+                counterTable[rows][col] += 1;              
+        }
+        //Copy current pressure value
+        prevPressureVal[rows][col] = pressureMat[rows][col];
+        
+        if(counterTable[rows][col] == MAXCOUNT){
+            //call inflated func. 
+            //TODO: Inflated func will will reset back
+       }
+    }
+   }   
+    
+ }   
     
 int main() {
-    float pressureMat[16][16];
+    
     while(1) {
         
             
-            //ait(3);
-            getPressureMatrix(pressureMat);
+            wait(0.05); // read pressure value after each 0.05 seconds
+            getPressureMatrix();
+            ComparePressureVal();
+            
         }
 }
