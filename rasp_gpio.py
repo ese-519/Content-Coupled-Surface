3e5b315ccfc4cd0460aef8e1975fbd2f383d5941
Chirag Shah <chirags@seas.upenn.edu>
2016-11-26 22:59:18 -0500

    Final faster optimized

--- rasp_gpio.py
+++ rasp_gpio.py
@@ -9,6 +9,7 @@ from matplotlib import cm
 import sys
 import numpy as np
 import thread
+from threading import Lock
 
 # scp rasp_gpio.py pi@10.251.87.217:/home/pi/Desktop/code-rep
 # http://10.251.87.217:8080/frontend.html
@@ -31,6 +32,9 @@ inflating = [10, 22]
 deflating = [27, 17]
 motors = [9, 11]
 cbar = None
+
+doneHeatMap = 0
+doneID = 0
 
 # Initializes input, output for raspberry pi BCM pins
 # be sure to init the select lines as well
@@ -96,66 +100,147 @@ def plot3DMatrix(matrix):
 	return xRows, yCols, height
 
 
-# initialize_raspi([], [enableDemux_pin], [0])
-# startPwm(0, 100)
-
-
 def createImage(xSet1, ySet1, level):
-	global fig
+	global mutex
 	global ax1
 	global matrix
-	fig = plt.figure(1)
-	setRowColumn(xSet1, ySet1, level, matrix)
+	global doneID
+	try:
+		mutex.acquire()
+		# fig = plt.figure(1)
+		for idx, xco in enumerate(xSet1):
+			setRowColumn(xco, ySet1[idx], level[idx], matrix)
 
-	xPos, yPos, dz = plot3DMatrix(matrix)
+		xPos, yPos, dz = plot3DMatrix(matrix)
 
-	zPos = [0 for i in range(0, len(dz))]
-	dx = np.ones(len(xPos))
-	dy = np.ones(len(yPos))
-	ax1.set_zlim(0, 0.5)
+		zPos = [0 for i in range(0, len(dz))]
+		dx = np.ones(len(xPos))
+		dy = np.ones(len(yPos))
+		ax1.set_zlim(0, 0.5)
 
-	colors = []
-	for i in range(0, len(zPos)):
-		if dz[i] > 0.3:
-			colors.append('b')
-		elif dz[i] > 0:
-			colors.append('r')
-		else:
-			colors.append('g')
-	 
-	ax1.bar3d(xPos, yPos, zPos, dx, dy, dz, color=colors)
-	plt.savefig('gen.png', bbox_inches='tight')	
+		colors = []
+		for i in range(0, len(zPos)):
+			if dz[i] > 0.3:
+				colors.append('b')
+			elif dz[i] > 0:
+				colors.append('r')
+			else:
+				colors.append('g')
+		 
+		ax1.bar3d(xPos, yPos, zPos, dx, dy, dz, color=colors)
+	finally:
+		'Created image for mat'
+		doneID = 1
+		mutex.release()
 
 
 def updateHeatMap(x, y, val):
-	global heatMapMatrix
-	global vmaxHeat
-	global axHeat
-	global fig
-	global cbar
-
-	fig = plt.figure(2)
-	heatMapMatrix[x][y] = val
-	if val > vmaxHeat:
-		vmaxHeat = val
-	labels= [i for i in range(0, len(heatMapMatrix[0]))]
-	
-	
-	heatmap = axHeat.pcolor(np.array(heatMapMatrix), cmap='hot', vmin=-10, vmax = vmaxHeat + 10, edgecolors='black')
-	if cbar != None:
-		cbar.remove()
-	cbar = fig.colorbar(heatmap)
-	#cbar.set_ticks(range(100)) # Integer colorbar tick locations
-	axHeat.set_xticklabels(labels, minor = False)
-	axHeat.set_yticklabels(labels, minor = False)
-	plt.savefig('heatMap.png', bbox_inches='tight')
+	try:
+		global mutex
+		global heatMapMatrix
+		global vmaxHeat
+		global axHeat
+		global fig
+		global cbar
+		global doneHeatMap
+		mutex.acquire()
+		fig = plt.figure(2)
+		for idx, xco in enumerate(x):
+			heatMapMatrix[xco][y[idx]] = val[idx]
+			if val[idx] > vmaxHeat:
+				vmaxHeat = val[idx]
+	finally:
+		print 'Inner heat map changed'
+		doneHeatMap = 1
+		mutex.release()
 
 
-def processHeatMap(threadName, listToHandle):
+def processHeatMap(listToHandle):
+	while 1:
+		if len(listToHandle) > 0:
+			xList = []
+			yList = []
+			valList = []
+			while len(listToHandle) > 0:	
+				parsed = listToHandle.pop(0)
+				# print 'Size of heat list' + str(len(listToHandle)) 
+				if parsed[0] == 'H':
+					xList.append(int(parsed[1]))
+					yList.append(int(parsed[2]))	
+					valList.append(int(parsed[3]))
+			# print 'Sent heat list for batch processing'
+			updateHeatMap(xList, yList, valList)
+		else:
+			time.sleep(0.1)
+
+def readData():
+	global listToHandleID, listToHandleHeat, ser
+	while 1:
+		x = ser.readline()
+		if x != "":
+			# print x
+			parsed = x.split(" ")
+			if len(parsed) == 2:
+				# print 'Added to Inf/def'
+				print x
+				listToHandleID.append(parsed)
+			elif len(parsed) == 4:
+				# print x
+				# print 'Added to heat map'
+				listToHandleHeat.append(parsed)
 
 
-def processInflateDeflate(threadName, listToHandle):
-	
+def processInflateDeflate(listToHandle):
+	global status_Matrix, inflation_deflation_delay, motors, inflating, deflating, def_delay
+	while 1:
+		if len(listToHandle) > 0:
+			xList = []
+			yList = []
+			valList = []
+			while len(listToHandle) > 0:
+				parsed = listToHandle.pop(0)
+				row = int(parsed[0])
+				columns = int(parsed[1])
+				val = 0
+				if status_Matrix[row][columns] == 0:
+					val = 0.1
+				if status_Matrix[row][columns] == 0:
+					print "Inflating"
+					status_Matrix[row][columns] = 1
+					if row == 0 and columns == 0:
+						# inflate this co-ordinate
+						GPIO.output(inflating[0], GPIO.HIGH)
+						GPIO.output(motors[0], GPIO.HIGH)
+						time.sleep(inflation_deflation_delay)
+						GPIO.output(inflating[0], GPIO.LOW)
+						GPIO.output(motors[0], GPIO.LOW)
+					elif row == 0 and columns == 1:
+						# inflate this co-ordinate
+						GPIO.output(inflating[1], GPIO.HIGH)
+						GPIO.output(motors[1], GPIO.HIGH)
+						time.sleep(inflation_deflation_delay)
+						GPIO.output(inflating[1], GPIO.LOW)
+						GPIO.output(motors[1], GPIO.LOW)
+
+				else:
+					print "Deflating"
+					status_Matrix[row][columns] = 0
+					if row == 0 and columns == 0:
+						# inflate this co-ordinate
+						GPIO.output(deflating[0], GPIO.HIGH)
+						time.sleep(def_delay)
+						GPIO.output(deflating[0], GPIO.LOW)
+					elif row == 0 and columns == 1:
+						# inflate this co-ordinate
+						GPIO.output(deflating[1], GPIO.HIGH)
+						time.sleep(def_delay)
+						GPIO.output(deflating[1], GPIO.LOW)
+				xList.append(row)
+				yList.append(columns)
+				valList.append(val)
+			createImage(xList, yList, valList)
+		else:
+			time.sleep(0.1)
 
 
 ser = serial.Serial(
@@ -178,11 +263,6 @@ heatMapMatrix = initZerosMatrix(16, 16)
 
 
 GPIO.setmode(GPIO.BCM)
-# init select lines
-# initialize_raspi([], selectLines, [0 for i in selectLines])
-# set output to be 0
-# initialize_raspi([], [demuxInput_pin], [0])
-# maybe have a demux for valves as well with same select lines but different output
 initialize_raspi([], [inflating], [0 for i in inflating])
 initialize_raspi([], [deflating], [0 for i in deflating])
 initialize_raspi([], [motors], [0 for i in motors])
@@ -190,99 +270,57 @@ ser.flushInput()
 listToHandleHeat = []
 listToHandleID = []
 
+mutex = Lock()
+# start the two threads
+thread.start_new_thread( processInflateDeflate, (listToHandleID, ) )
+thread.start_new_thread( processHeatMap, (listToHandleHeat, ) )
+thread.start_new_thread( readData, ())
 
 while 1:
-	x = ser.readline()
-	if x != "":
-		print x
-		parsed = x.split(" ")
-		if len(parsed) == 2:
-			listToHandleID.append(parsed)
-			row = int(parsed[0])
-			columns = int(parsed[1])
-			if status_Matrix[row][columns] == 0:
-				print "Inflating"
-				status_Matrix[row][columns] = 1
-				createImage(row, columns, 0.1)
-				if row == 0 and columns == 0:
-					# inflate this co-ordinate
-					GPIO.output(inflating[0], GPIO.HIGH)
-					GPIO.output(motors[0], GPIO.HIGH)
-					time.sleep(inflation_deflation_delay)
-					GPIO.output(inflating[0], GPIO.LOW)
-					GPIO.output(motors[0], GPIO.LOW)
-				elif row == 0 and columns == 1:
-					# inflate this co-ordinate
-					GPIO.output(inflating[1], GPIO.HIGH)
-					GPIO.output(motors[1], GPIO.HIGH)
-					time.sleep(inflation_deflation_delay)
-					GPIO.output(inflating[1], GPIO.LOW)
-					GPIO.output(motors[1], GPIO.LOW)
+	# time.sleep(3)
+	mutex.acquire()
+	if doneID == 1:
+		print 'Saving new Images'
+		plt.figure(1)
+		plt.savefig('gen.png', bbox_inches='tight')
+		doneID = 0
+	if doneHeatMap == 1:	
+		print 'Saving new Images'
+		plt.figure(2)
+		heatmap = axHeat.pcolor(np.array(heatMapMatrix), cmap='hot', vmin=-10, vmax = vmaxHeat + 10, edgecolors='black')
+		if cbar != None:
+			cbar.remove()
+		cbar = fig.colorbar(heatmap)
+		labels= [i for i in range(0, len(heatMapMatrix[0]))]
+		#cbar.set_ticks(range(100)) # Integer colorbar tick locations
+		axHeat.set_xticklabels(labels, minor = False)
+		axHeat.set_yticklabels(labels, minor = False)
+		plt.savefig('heatMap.png', bbox_inches='tight')
+		doneHeatMap = 0;
+	mutex.release()
 
-			else:
-				createImage(row, columns, 0)
-				print "Deflating"
-				status_Matrix[row][columns] = 0
-				if row == 0 and columns == 0:
-					# inflate this co-ordinate
-					GPIO.output(deflating[0], GPIO.HIGH)
-					time.sleep(def_delay)
-					GPIO.output(deflating[0], GPIO.LOW)
-				elif row == 0 and columns == 1:
-					# inflate this co-ordinate
-					GPIO.output(deflating[1], GPIO.HIGH)
-					time.sleep(def_delay)
-					GPIO.output(deflating[1], GPIO.LOW)
-			ser.flushInput()
-		elif len(parsed) == 4:
-			if parsed[0] == 'H':
-				x = int(parsed[1])
-				y = int(parsed[2])
-				val = int(parsed[3])
-				updateHeatMap(x, y, val)
-				ser.flushInput()
 
 # while 1:
-# 	x = ser.readline()
-# 	if x != "":
-# 		print x
-# 		parsed = x.split(" ")
-# 		if len(parsed) == 2:
-# 			row = int(parsed[0])
-# 			column = int(parsed[1])
-# 			if status_Matrix[row][columns] == 0:
-# 				val = parsed[0]*4 + parsed[1]
-# 				setSelectLinesTo(val)
-# 				status_Matrix[row][columns] = 1
-# 				createImage(row, columns, 0.1)
-# 				GPIO.output(demuxInput_pin, GPIO.HIGH)
-# 				time.sleep(inflation_deflation_delay)
-# 				GPIO.output(demuxInput_pin, GPIO.LOW)
-# 			else:
-# 				createImage(row, columns, 0)
-# 				status_Matrix[row][columns] = 0
 
-while 1:
-
-	# Maybe test it with the user input 1st
-	# get the number to input
-	inp = raw_input('Enter the selectline number\n')
-	if inp == 'quit':
-		exit()
-		GPIO.cleanup()
-	try:
-		val = int(inp)
-		# select the select lines
-		setSelectLinesTo(val)
-		# start PWM
-		pwmObj.stop()
-		startPwm(75, 100)
-		time.sleep(3)
-		pwmObj.stop()
-		startPwm(0, 100)
-	except ValueError:
-		print('Please enter a number')
-	time.sleep(1)
+# 	# Maybe test it with the user input 1st
+# 	# get the number to input
+# 	inp = raw_input('Enter the selectline number\n')
+# 	if inp == 'quit':
+# 		exit()
+# 		GPIO.cleanup()
+# 	try:
+# 		val = int(inp)
+# 		# select the select lines
+# 		setSelectLinesTo(val)
+# 		# start PWM
+# 		pwmObj.stop()
+# 		startPwm(75, 100)
+# 		time.sleep(3)
+# 		pwmObj.stop()
+# 		startPwm(0, 100)
+# 	except ValueError:
+# 		print('Please enter a number')
+# 	time.sleep(1)
 
 
 # def PlotHeatMap(x,y,val):
