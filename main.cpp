47ba01308a5bee582b7a85277d8fcff95b83275a
Chirag Shah <chirags@seas.upenn.edu>
2016-11-28 19:46:02 -0500

    Uploading new main

--- main.cpp
+++ main.cpp
@@ -2,12 +2,18 @@
 
 
 Serial pc(USBTX, USBRX);
+// serial pins for communication with Raspberry
+Serial mySerial(p9, p10);
 
-#define MAXCOUNT 100
+#define MAXCOUNT 5
 #define LEN 16
-#define THRESHOLD 20
+#define THRESHOLD 10 // if user hasn't moved above this point inflate
 #define WINDOW 4
+#define PRESSURE_THRESHOLD 30.0 // if the pressure is less than this set 0
+#define AVERAGE_PRESSURE_THRESHOLD 10.0 // if the average of a WINDOW*WINDOW is less than this don't consider it to inflate
+#define HEAT_THRESH 5
 
+int sendCounter = 0;
 float prevPressureVal[LEN][LEN]={0};
 float pressureMat[LEN][LEN] = {0};
 int counterTable[LEN][LEN] = {0};
@@ -56,12 +62,24 @@ void getPressureMatrix(){
             select2_mux1 = getBit(col, 2);
             select3_mux1 = getBit(col, 3);
             //wait(0.05);
-            pressureMat[rows][col] = analog_read_1.read()*100;
-            printf("%f ", pressureMat[rows][col]);
+            float x = analog_read_1.read()*100;
+            float val = x;
+            if(val < PRESSURE_THRESHOLD) {
+                val = 0.0;
+            }
+            pressureMat[rows][col] = val;
+//            if(abs((int)pressureMat[rows][col] - (int)prevPressureVal[rows][col]) > HEAT_THRESH) {
+//                mySerial.printf("H %d %d %d\n", rows, col, (int)pressureMat[rows][col]);
+//                printf("Sent for heat map\n");
+//            }
+            if( sendCounter == 0) {
+                mySerial.printf("H %d %d %d\n", rows, col, (int)x);
+            }
         
         }  
-        printf("\n");
+//        printf("\n");
     }
+    sendCounter  = (sendCounter + 1)%4;
     printf("********************************\n");
 }
 
@@ -75,32 +93,26 @@ void ComparePressureVal(){
     for(int col = 0; col < LEN; col++){
        //TODO: the pressure might differ a bit but could be in same range. We might want to consider
        // this situation too.
-       if(abs(pressureMat[rows][col] - prevPressureVal[rows][col]) < THRESHOLD){
-                counterTable[rows][col] += 1;              
+       if(abs((int)pressureMat[rows][col] - (int)prevPressureVal[rows][col]) > HEAT_THRESH) {
+            //mySerial.printf("H %d %d %d\n", rows, col, (int)pressureMat[rows][col]);
         }
-        //Copy current pressure value
-        prevPressureVal[rows][col] = pressureMat[rows][col];
-        
-        if(counterTable[rows][col] == MAXCOUNT){
-            //call inflated func. 
-            //TODO: Inflated func will will reset back
-       }
+            prevPressureVal[rows][col] = pressureMat[rows][col];
     }
-   }   
-    
- }
+}
+}
 
 
  void comparePressureOnAnAverage() {
     for(int rows = 0; rows < LEN; rows = rows + WINDOW) {
         for(int col = 0; col < LEN; col = col + WINDOW) {
-            int avg = 0;
+            float avg = 0;
             for (int i = rows; i < rows + WINDOW; i++) {
                 for (int j = col; j < col + WINDOW; j++) {
                         avg += pressureMat[i][j];
                 }
             }
-            avg = avg/(WINDOW*WINDOW);
+            avg = (avg/(WINDOW*WINDOW))*1.3;
+            if(avg > AVERAGE_PRESSURE_THRESHOLD) {
             if(abs(previousRead[rows/4][col/4] - avg) < THRESHOLD) {
                 counterRead[rows/4][col/4] += 1;
                 if(counterRead[rows/4][col/4] == MAXCOUNT) {
@@ -108,18 +120,29 @@ void ComparePressureVal(){
                     // inflate the area automatically
                     // send signal to the pins of the mbed to actually infalte that area for row/4 and col/4
                     // reset the matrix back to zeros since this node has been inflated
-                }
+                    pc.printf("Sent to RaspPi %d %d\n", rows/4, col/4);
+                    mySerial.printf("%d %d\n", rows/4, col/4);
+                    counterRead[rows/4][col/4] = 0;                }
+            } else {
+                counterRead[rows/4][col/4] = 0;
             }
+            } else {
+                avg = 0.0;    
+            }
+            printf("%f ", avg);
             previousRead[rows/4][col/4] = avg;
         }
+        printf("\n");
     }
+    printf("********************************\n");
  }   
     
 int main() {
-    
+    mySerial.baud(9600);
     while(1) {
             wait(1); // read pressure value after each 0.05 seconds
             getPressureMatrix();
-            ComparePressureVal();
+//            ComparePressureVal();
+            comparePressureOnAnAverage();
         }
 }
